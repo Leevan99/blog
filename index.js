59b952017060e08504a92aaff666b95ba2ce1700
CeravoloLuca <lucaceravolo99@gmail.com>
2024-12-17 19:33:11 +0100

    Revert "Fix error"
    This reverts commit db5c303c55958cd2fa5b125f9ce5fdc2cae68639.

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express")
 const http = require("http")
 const routes = require("./routes")
 const path = require("path")
-const db = require('./db/db')
-
-
 
 const app = express()
 const port = process.env.port || 3000
@@ -17,13 +14,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static( path.join(__dirname, 'public')))
 
 
 
-
-
-app.get('/', db.articles, routes.homepage)
+app.get('/', routes.homepage)
 
 const server = http.createServer(app)
 server.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@
 
 
 exports.homepage = (req, res, next)=>{
-    const articles = req.articles
     res.render('homepage', {pageTitle: "homepage", articles})
 }
